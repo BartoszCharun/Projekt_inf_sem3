7fa32a680eefeedb3f6b23fe8edebc2303a0bb8e
BartoszCharun <bartek.charun@op.pl>
2025-11-29 13:13:28 +0100

    Paddle 2

--- paddle.h
+++ paddle.h
@@ -1,17 +1,22 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-class Paletka {
+class paletka {
 private:
     sf::RectangleShape shape;
     float speed = 6.f;
 
 public:
-    Paletka(float x, float y) {
+    paletka(float x, float y) {
         shape.setSize({100.f, 20.f});
         shape.setFillColor(sf::Color(229, 127, 61));
         shape.setPosition(sf::Vector2f(x, y));
     }
+
+    void setPosition(float x, float y) {
+        shape.setPosition(sf::Vector2f(x, y));
+    }
+    
 
     void moveLeft() {
         shape.move(sf::Vector2f(-speed, 0.f));
@@ -32,11 +37,18 @@ public:
         if (pos.x + size.x > 800) shape.setPosition(sf::Vector2f(800.f - size.x, pos.y));
     }
 
-    void draw(sf::RenderWindow& window) {
-        window.draw(shape);
+    void draw(sf::RenderTarget& target) {
+        target.draw(shape);
     }
+    
 
     sf::FloatRect getBounds() const {
         return shape.getGlobalBounds();
     }
+
+    sf::Vector2f getPosition() const {
+        return shape.getPosition();
+    }
+
+    
 };
